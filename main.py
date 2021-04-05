2b3e1b5ce35d83a20560daa69bc53322f9279c4e
Krzysztof Nalepa <misiek.nalepa@wp.pl>
2021-04-05 12:11:13 +0200

    flask added :d

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 import cv2
+from flask import Flask, render_template, Response
 
-cap = cv2.VideoCapture(0)
+app = Flask(__name__)
+camera = cv2.VideoCapture(0)
 
-while True:
 
-    ret, frame = cap.read()
+def gen_frames():
+    while True:
+        success, frame = camera.read()  # read the camera frame
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
 
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Display the resulting frame
-    cv2.imshow('frame',gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    app.run(debug=True)

87a2c3bf1090457a732f55d1fc3da0a5c8ba3429
Krzysztof Nalepa <misiek.nalepa@wp.pl>
2021-04-05 13:11:29 +0200

    flask version working

--- main.py
+++ main.py
@@ -1,19 +1,35 @@
+from flask import Flask, render_template, Response
+import io
 import cv2
 
-cap = cv2.VideoCapture(0)
+app = Flask(__name__)
 
-while True:
 
-    ret, frame = cap.read()
+@app.route('/')
+def index():
+    """Video streaming home page."""
+    return render_template('index.html')
 
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Display the resulting frame
-    cv2.imshow('frame',gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+def gen():
+    vc = cv2.VideoCapture(0)
+    """Video streaming generator function."""
+    while True:
+        read_return_code, frame = vc.read()
+        encode_return_code, image_buffer = cv2.imencode('.jpg', frame)
+        io_buf = io.BytesIO(image_buffer)
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + io_buf.read() + b'\r\n')
 
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+
+@app.route('/video_feed')
+def video_feed():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(
+        gen(),
+        mimetype='multipart/x-mixed-replace; boundary=frame'
+    )
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True, threaded=True)

46f609b107f15927a52acc8dd73157a1a8e5b793
Krzysztof Nalepa <misiek.nalepa@wp.pl>
2021-04-05 13:48:09 +0200

    auuuau

--- reciever.py
+++ reciever.py
@@ -1,10 +1,6 @@
 import socket
-import tkinter as tk
-import tkinter as tk
 import numpy as np
-from PIL import Image, ImageTk
-
-
+import cv2 as cv
 
 addr = ("127.0.0.1", 65534)
 buf = 512
@@ -16,27 +12,21 @@ num_of_chunks = width * height * 3 / buf
 if __name__ == '__main__':
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.bind(addr)
-    # while True:
-    chunks = []
-    start = False
-    while len(chunks) < num_of_chunks:
-        chunk, _ = s.recvfrom(buf)
-        if start:
-            chunks.append(chunk)
-        elif chunk.startswith(code):
-            start = True
+    while True:
+        chunks = []
+        start = False
+        while len(chunks) < num_of_chunks:
+            chunk, _ = s.recvfrom(buf)
+            if start:
+                chunks.append(chunk)
+            elif chunk.startswith(code):
+                start = True
 
-    byte_frame = b''.join(chunks)
+        byte_frame = b''.join(chunks)
 
-    frame = np.frombuffer(
-        byte_frame, dtype=np.uint8).reshape(height, width, 3)
+        frame = np.frombuffer(
+            byte_frame, dtype=np.uint8).reshape(height, width, 3)
 
-    root = tk.Tk()
-
-    img = ImageTk.PhotoImage(image=Image.fromarray(frame))
-
-    canvas = tk.Canvas(root)
-    canvas.pack()
-    canvas.create_image(20, 20, anchor="nw", image=img)
-
-    root.mainloop()
+        cv.imshow('recv', frame)
+        if cv.waitKey(1) & 0xFF == ord('q'):
+            break
